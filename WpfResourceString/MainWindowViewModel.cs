3aef6515709e56d168009cce284ab7a995f4b774
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2021-10-25 18:40:03 +0900

    構成を変更。

---
+++ WpfResourceString/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfResourceString
+{
+    public class MainWindowViewModel
+    {
+        /// <summary>
+        /// 「さようなら」メッセージ。
+        /// 「Properties.Resources.GOODBYE」を参照。
+        /// </summary>
+        public string GoodBye { get; set; } = Properties.Resources.GOODBYE;
+    }
+}
--- WpfResourceString/Properties/Resources.Designer.cs
+++ WpfResourceString/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WpfResourceString.Resources {
+namespace WpfResourceString.Properties {
     using System;
     
     
@@ -22,14 +22,14 @@ namespace WpfResourceString.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resource {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resource() {
+        internal Resources() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace WpfResourceString.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WpfResourceString.Resources.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WpfResourceString.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -57,6 +57,15 @@ namespace WpfResourceString.Resources {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   good-bye に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string GOODBYE {
+            get {
+                return ResourceManager.GetString("GOODBYE", resourceCulture);
             }
         }
         
